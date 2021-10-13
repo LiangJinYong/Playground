a61accd38d34b3cb1447d3bb70ecd8a24f764dd3
swetg100@gmail.com <swetg100@gmail.com>
2021-10-13 13:43:40 +0900

    초기 커밋.

---
+++ Playground/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+
+            for (int i = 0; i < nics.Length; i++)
+            {
+                Console.WriteLine(i + " ==> " + nics[i].GetPhysicalAddress().ToString() + " -- " + nics[i].OperationalStatus);
+            }
+        }
+    }
+}
